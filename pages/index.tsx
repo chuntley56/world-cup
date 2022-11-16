58e63a78b7139ce94001f51ec7c842c926ce6943
Corey Huntley <corey.huntley@dollarshaveclub.com>
2022-11-15 16:17:20 -0800

    add fixtures component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,20 +3,6 @@ import Image from 'next/image'
 import Fixtures from 'components/Fixtures.tsx'
 
 const App = () => {
-  // const options = {
-  //   method: 'GET',
-  //   headers: {
-  //     'X-RapidAPI-Key': 'b84ccf0588msh538de6092fff9b7p1a32d9jsn5539e74ecf8c',
-	// 	  'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
-  //   }
-  // }
-
-  // const getData = (url, options) => {
-  //   fetch(`https://api-football-v1.p.rapidapi.com/v3/${url}?league=1&season=2022`, options)
-  //   .then(response => response.json())
-  //   .then(response => console.log(response))
-  //   .catch(err => console.error(err));
-  // }
 
 
 
